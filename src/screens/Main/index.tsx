c3c4cef1deea778590d4054933a2d1047df1a3a4
Lucas Lima <lucas.lldcosta@gmail.com>
2022-08-12 18:51:44 -0300

    constinue dev

--- src/screens/Main/index.tsx
+++ src/screens/Main/index.tsx
@@ -4,7 +4,7 @@ import { Entypo } from "@expo/vector-icons";
 import * as S from "./styles";
 
 import logo from "../../assets/images/logo.png";
-import airplane from "../../assets/images/ariplane.png";
+import airplane from "../../assets/images/airplane.png";
 
 export default function Main() {
   return (
@@ -30,9 +30,30 @@ export default function Main() {
               <S.SmallText>20:45</S.SmallText>
             </S.TextContent>
           </S.TextRowContent>
-          <S.Airplane source={airplane} />
+          <S.Airplane source={airplane} resizeMode="contain" />
         </S.Content>
       </S.FlyInfo>
+      <S.TicketInfo>
+        <S.TicketView>
+          <S.Desc>Flight</S.Desc>
+          <S.Value>AR 580</S.Value>
+        </S.TicketView>
+        <S.TicketView>
+          <S.Desc>Class</S.Desc>
+          <S.Value>Premium{"\n"}Econim</S.Value>
+        </S.TicketView>
+        <S.TicketView>
+          <S.Desc>Aircraft</S.Desc>
+          <S.Value>AR 580</S.Value>
+        </S.TicketView>
+        <S.TicketView>
+          <S.Desc>Possibilty</S.Desc>
+          <S.Value>AR 580</S.Value>
+        </S.TicketView>
+      </S.TicketInfo>
+      <S.Value>
+        43h 15m <S.Desc>total duration</S.Desc>
+      </S.Value>
     </S.Container>
   );
 }
--- src/screens/Main/styles.ts
+++ src/screens/Main/styles.ts
@@ -6,20 +6,23 @@ interface Props {
   alingment?: "left" | "right";
   mt?: number;
   bold?: boolean;
+  noResize?: boolean;
 }
 
 export const Container = styled.SafeAreaView`
   flex: 1;
+  align-items: center;
 `;
 
-export const FlyInfo = styled.View`
+export const FlyInfo = styled(Animated.View)`
   width: 100%;
-  height: 45%;
+  height: 40%;
+  overflow: hidden;
 `;
 
 export const Content = styled(LinearGradient).attrs({
   colors: ["#0438AE", "#0438AE", "#859DDF"],
-  start: { x: 0.5, y: -0.5 },
+  start: { x: 0.5, y: -0.6 },
   end: { x: 0.5, y: 1 },
 })`
   width: 100%;
@@ -63,7 +66,39 @@ export const LargeText = styled.Text`
   font-size: 30px;
 `;
 
-export const Airplane = styled.Image`
-  height: 30px;
-  width: 180px;
+export const Airplane = styled(Animated.Image)`
+  height: 200px;
+  width: 360px;
+  position: absolute;
+  align-self: center;
+  bottom: -30px;
+`;
+
+export const TicketInfo = styled.View`
+  width: 100%;
+  height: 80px;
+  flex-direction: row;
+  background: white;
+  align-items: center;
+  box-shadow: 0px 4px 20px rgba(0, 0, 0, 0.1);
+  margin-bottom: 30px;
+`;
+
+export const TicketView = styled.View`
+  flex: 1;
+  height: 100%;
+  align-items: center;
+  justify-content: flex-start;
+  padding-top: 15px;
+`;
+
+export const Desc = styled.Text`
+  color: gray;
+  margin-bottom: 3px;
+  width: 60%;
+`;
+
+export const Value = styled.Text`
+  color: black;
+  width: 60%;
 `;
