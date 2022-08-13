7d5b48d745b308609b6d4d84dbbff7ec69c6bb08
Lucas Lima <lucas.lldcosta@gmail.com>
2022-08-13 16:56:05 -0300

    finish cards

--- src/screens/Main/components/CardSelect/index.tsx
+++ src/screens/Main/components/CardSelect/index.tsx
@@ -10,22 +10,41 @@ import {
 
 import * as S from "./styles";
 
+import { cards } from "./utils";
+
 export default function CardSelect() {
   const buttonScale = useSharedValue(1);
 
-  const buttonAnimatedStyle = useAnimatedStyle(() => ({
-    transform: [{ scale: buttonScale.value }],
+  const cardAnimatedStyle = useAnimatedStyle(() => ({
+    transform: [{ perspective: 400 }, { scale: buttonScale.value }, { rotateX: "50deg" }],
+    marginBottom: -140,
   }));
 
   return (
     <>
       <S.ButtonView entering={ZoomIn.delay(300)}>
         <S.Button>
-          <Entypo name="plus" size={24} color="#ddd" />
+          <Entypo name="plus" size={26} color="#ddd" />
         </S.Button>
       </S.ButtonView>
       <S.Container entering={FlipInXDown.duration(600)}>
-        <S.Title>SELECT PAYMENT MEHOD</S.Title>
+        <S.Title>SELECT PAYMENT METHOD</S.Title>
+        <S.CardScroll>
+          {cards.map((card) => (
+            <S.Card key={card.id} style={cardAnimatedStyle}>
+              <S.CardContent colors={card.colors}>
+                <S.TopCardLogo source={card.topLogo} resizeMode="contain" />
+                <S.BottomCardView>
+                  <S.CardTextView>
+                    <S.CardNumber>{card.number}</S.CardNumber>
+                    <S.CardName>ANDREW MITCHEL</S.CardName>
+                  </S.CardTextView>
+                  <S.BottomCardLogo source={card.bottomLogo} resizeMode="contain" />
+                </S.BottomCardView>
+              </S.CardContent>
+            </S.Card>
+          ))}
+        </S.CardScroll>
       </S.Container>
     </>
   );
---
+++ src/screens/Main/components/CardSelect/utils/index.ts
@@ -0,0 +1,53 @@
+const opacity = 0.92;
+
+export const cards = [
+  {
+    id: 1,
+    topLogo: require("../../../../../assets/images/marcusLogo.png"),
+    bottomLogo: require("../../../../../assets/images/mastercard.png"),
+    colors: [`rgba(224, 111, 197, ${opacity})`, `rgba(221, 73, 89, ${opacity})`],
+    number: 9945,
+  },
+  {
+    id: 2,
+    topLogo: require("../../../../../assets/images/virgin.png"),
+    bottomLogo: require("../../../../../assets/images/mastercard.png"),
+    colors: [`rgba(164, 51, 51, ${opacity})`, `rgba(164, 51, 51, ${opacity})`],
+    number: 2135,
+  },
+  {
+    id: 3,
+    topLogo: require("../../../../../assets/images/americanExpress.png"),
+    bottomLogo: require("../../../../../assets/images/visa.png"),
+    colors: [`rgba(128, 128, 128, ${opacity})`, `rgba(89, 89, 90, ${opacity})`],
+    number: 1234,
+  },
+  {
+    id: 4,
+    topLogo: require("../../../../../assets/images/delta.png"),
+    bottomLogo: require("../../../../../assets/images/visa.png"),
+    colors: [`rgba(126, 184, 253, ${opacity})`, `rgba(172, 157, 252, ${opacity})`],
+    number: 4587,
+  },
+  {
+    id: 5,
+    topLogo: require("../../../../../assets/images/americanExpress.png"),
+    bottomLogo: require("../../../../../assets/images/visa.png"),
+    colors: ["rgba(126, 184, 253, ${opacity})", "rgba(172, 157, 252, ${opacity})"],
+    number: 8765,
+  },
+  {
+    id: 6,
+    topLogo: require("../../../../../assets/images/americanExpress.png"),
+    bottomLogo: require("../../../../../assets/images/visa.png"),
+    colors: [`rgba(128, 128, 128, ${opacity})`, `rgba(89, 89, 90, ${opacity})`],
+    number: 4563,
+  },
+  {
+    id: 7,
+    topLogo: require("../../../../../assets/images/virgin.png"),
+    bottomLogo: require("../../../../../assets/images/mastercard.png"),
+    colors: [`rgba(164, 51, 51, ${opacity})`, `rgba(164, 51, 51, ${opacity})`],
+    number: 2135,
+  },
+];
